cab4e90e24407b290e602aef85cfacc4fc4d4a5d
Garfield S <gary.9306@gmail.com>
2021-05-09 09:35:45 -0400

    exception.01

---
+++ 01-java-fundamentals/08-lists-of-exceptions/ExceptionPractice.java
@@ -0,0 +1,18 @@
+public class ExceptionPractice{
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+            myList.add("13");
+            myList.add("hello world");
+            myList.add(48);
+            myList.add("Goodbye World");
+
+        try{ 
+            for(int i = 0; i < myList.size(); i++) {
+              Integer castedValue = (Integer) myList.get(i);
+              System.out.println("Integer Value");
+        } catch (){
+            System.out.println("Not an Integer value");
+        }
+        }//end Try
+    }// end main
+}
