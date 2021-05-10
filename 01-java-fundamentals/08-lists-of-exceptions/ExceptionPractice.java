82932b688c291c53e034e4583f2bbedfba0e97ad
Garfield Simms III <gary.9306@gmail.com>
2021-05-10 08:03:59 -0400

    Exception.02

--- 01-java-fundamentals/08-lists-of-exceptions/ExceptionPractice.java
+++ 01-java-fundamentals/08-lists-of-exceptions/ExceptionPractice.java
@@ -1,18 +1,25 @@
+import java.util.ArrayList;
 public class ExceptionPractice{
     public static void main(String[] args) {
         ArrayList<Object> myList = new ArrayList<Object>();
-            myList.add("13");
+            myList.add(13);
             myList.add("hello world");
             myList.add(48);
             myList.add("Goodbye World");
 
-        try{ 
+        try {
             for(int i = 0; i < myList.size(); i++) {
-              Integer castedValue = (Integer) myList.get(i);
-              System.out.println("Integer Value");
-        } catch (){
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println("Integer Value");
+            
+        } 
+            
+        } catch (Exception e) {
             System.out.println("Not an Integer value");
         }
-        }//end Try
-    }// end main
+            
+           
+        
+        
+        }
 }
